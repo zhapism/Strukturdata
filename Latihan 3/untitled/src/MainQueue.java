18dccf378b1a4210c2c1c2e48185f251bcf4c836
zhapism <106977762+zhapism@users.noreply.github.com>
2023-04-04 13:20:09 +0700

    Add files via upload

---
+++ Latihan 3/untitled/src/MainQueue.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MainQueue {
+    public static void queueExample() {
+        Queue queue = new LinkedList();
+        queue.add("Java");
+        queue.add("DotNet");
+        queue.offer("PHP");
+        queue.offer("HTML");
+        System.out.println("remove : " + queue.remove());
+        System.out.println("element : " + queue.element());
+        System.out.println("poll : " + queue.poll());
+        System.out.println("peek : " + queue.peek());
+    }
+
+    public static void main(String[] args) {
+        new MainQueue().queueExample();
+    }
+}
